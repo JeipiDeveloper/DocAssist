184aa79f54b36a736441974088fdf63c41b31fd2
JPedrex <joaosin.cae@hotmail.com>
2026-05-20 19:58:45 -0300

    Endpoint para upload de PDF

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const storage = multer.diskStorage({
+
+    destination: function(req, file, cb) {
+        cb(null, "uploads/");
+    },
+
+    filename: function(req, file, cb) {
+        cb(null, Date.now() + "-" + file.originalname);
+    }
+
+});
+
+const upload = multer({ storage });
+
+app.post("/upload", upload.single("pdf"), (req, res) => {
+
+    console.log(req.file);
+
+    res.json({
+        message: "PDF enviado com sucesso"
+    });
+
+});
+
+app.listen(3000, () => {
+    console.log("Servidor rodando");
+});
